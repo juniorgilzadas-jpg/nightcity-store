35cfbef4a1f8056ea4a161a91d78fc2f25d97f35
juniorgilzadas-jpg <juniorgilzadas@gmail.com>
2026-06-03 17:02:53 +0200

    Update NIGHTCITY_API URL for deployment

--- site/config.js
+++ site/config.js
@@ -1,3 +1,1 @@
-// Apos publicar o bot no Render, cole a URL aqui (sem barra no final):
-// Exemplo: window.NIGHTCITY_API = "https://nightcity-bot.onrender.com";
-window.NIGHTCITY_API = "";
+window.NIGHTCITY_API = "https://nightcity-store.onrender.com";
